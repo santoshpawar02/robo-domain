62ad90196775518e92f8ac5d196a25e40289234b
santoshpawar02 <santosh.pawar02@gmail.com>
2025-02-12 16:08:48 +0530

    files added

---
+++ mongodb.sh
@@ -0,0 +1,10 @@
+cp -r mongo.repo /etc/yum.repos.d/mongo.repo
+
+dnf install mongodb-org -y 
+
+systemctl enable mongod 
+systemctl start mongod 
+
+sed -i "s/127.0.0.1/0.0.0.0/" /etc/mongod.conf
+
+systemctl restart mongod
